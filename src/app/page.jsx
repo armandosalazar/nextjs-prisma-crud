577a36126c803f0a9d5c7910b63b26bc4e5b31f7
Armando Salazar <armandosalazar351@gmail.com>
2023-12-26 12:32:58 -0600

    refactor: refactor home page component and add task loading functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,36 @@
-export default function Home() {
+import Prisma from '@/libs/prisma';
+
+async function loadTask() {
+  // get data with fetch
+  // const res = await fetch('http://localhost:3000/api/tasks');
+  // const data = await res.json();
+  // console.log(data);
+
+  // get data with prisma
+  return await Prisma.task.findMany();
+}
+
+export default async function Home() {
+  const tasks = await loadTask();
+  console.log(tasks);
+
   return (
-    <main>
-      <h1>Home page</h1>
+    <main className="container mx-auto">
+      <h1 className="text-3xl font-bold my-5">Home page</h1>
+      <section className="grid grid-cols-3 gap-3">
+        {tasks.map((task) => {
+          return (
+            <article
+              key={task.id}
+              className="bg-slate-900 p-5 hover:bg-slate-800 hover:cursor-pointer"
+            >
+              <h2 className="text-2xl font-bold">{task.title}</h2>
+              <p>{task.description}</p>
+              <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+            </article>
+          );
+        })}
+      </section>
     </main>
   );
 }
