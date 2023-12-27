4bb37e711fbdc9f8cc8f4dec0f03c4708f4e6192
Armando Salazar <armandosalazar351@gmail.com>
2023-12-27 11:59:38 -0600

    refactor: AboutPage component to include additional content

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,20 @@
 function AboutPage() {
   return (
-    <main className="m-4">
-      <h1>About</h1>
+    <main className="container mx-auto">
+      <h1 className="text-3xl font-bold my-4">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        assumenda, eveniet ullam exercitationem culpa excepturi eligendi alias,
+        deserunt provident ad modi sapiente. Nisi autem sunt fugiat atque aut.
+        Nostrum rem impedit recusandae error ut odit iure ducimus suscipit
+        dolores dolor inventore iusto nobis ex non quidem esse fuga et, earum
+        perferendis explicabo itaque eius minus blanditiis? Earum omnis tempora
+        dolore iusto labore alias dicta sed aspernatur itaque voluptatum quasi
+        qui, nesciunt, quaerat laborum illo nihil eaque sint. Nemo eligendi quia
+        placeat itaque blanditiis numquam quod, quaerat sit cumque quisquam
+        consequuntur, esse iste distinctio officiis ipsum sed ab accusantium
+        dicta non!
+      </p>
     </main>
   );
 }
